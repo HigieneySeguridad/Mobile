80a1479c83ea2a53b4bb5f071b0bf225da79da22
nico <nicoveron28@gmail.com>
2023-10-25 15:22:33 -0300

    login funkando segun pani

--- screens/Login/Login.js
+++ screens/Login/Login.js
@@ -2,6 +2,104 @@ import React, { useState } from "react";
 import {  View,  TextInput } from "react-native";
 import { styles } from "./LoginStyle";
 import Boton from "../../components/boton"
+import { useNavigation } from "@react-navigation/native";
+import { ALERT_TYPE, Dialog} from 'react-native-alert-notification';
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigation = useNavigation();
+
+    const Ingresar = async (e) => {
+        e.preventDefault();
+
+        if(username.trim()=== "" || password.trim()=== ""){
+            Dialog.show({
+                type: ALERT_TYPE.DANGER,
+                title: "Error",
+                textBody: "Debe completar todos los campos",
+                button: "Aceptar",
+                closeOnOverlayTap: true,
+                });
+            }
+        const iniciarSesion = {
+            username,
+            password
+        }
+
+        const respuesta = await fetch('http://192.168.0.181:3000/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(iniciarSesion),
+          });
+        
+          if (respuesta.status === 200) {
+            Dialog.show({
+                type: ALERT_TYPE.SUCCESS,
+                title: "Correcto",
+                textBody: "Iniciaste sesion",
+                button: "Aceptar",
+                closeOnOverlayTap: true,
+                });
+            navigation.navigate("Home")// Redirigir al usuario solo cuando las credenciales son válidas
+          } 
+    
+          if (respuesta.status ===401){
+            Dialog.show({
+                type: ALERT_TYPE.DANGER,
+                title: "ERROR",
+                textBody: "REVISE LOS CAMPOS",
+                button: "Aceptar",
+                closeOnOverlayTap: true,
+                });
+           console.log(await respuesta.json())
+          } 
+          if (respuesta.status === 429){
+            Dialog.show({
+                type: ALERT_TYPE.WARNING,
+                title: "ESPERE",
+                textBody: "Espere 1 minuto para realizar mas intentos",
+                button: "Aceptar",
+                closeOnOverlayTap: true,
+                });
+            console.log(await respuesta.text())
+          }
+
+    }
+    
+    return (
+        <View style={styles.container}>
+            <TextInput 
+            style={styles.input}
+            placeholder="Usuario"
+            placeholderTextColor={"#AAAAAA"}
+            onChangeText={setUsername}
+            value={username}
+            
+            />
+            <TextInput
+            style={styles.input}
+            placeholder="Contraseña"
+            placeholderTextColor={"#AAAAAA"}
+            onChangeText={setPassword}
+            value={password}
+            
+            />
+            <Boton text="Ingresar" apretame={Ingresar} />
+         
+        </View>
+        )
+    
+    
+}
+export default Login;
+
+/* import React, { useState } from "react";
+import {  View,  TextInput } from "react-native";
+import { styles } from "./LoginStyle";
+import Boton from "../../components/boton"
 import { ALERT_TYPE, Dialog} from 'react-native-alert-notification';
 import { useNavigation } from "@react-navigation/native";
 
@@ -54,4 +152,4 @@ const Login = () => {
     
     
 }
-export default Login;
+export default Login; */
