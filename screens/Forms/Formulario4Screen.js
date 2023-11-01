19d428be14eae8ec26d68a6791196de645f0774d
LautaDurante <durantelautarop@gmail.com>
2023-11-01 14:35:16 -0300

    probando 4

--- screens/Forms/Formulario4Screen.js
+++ screens/Forms/Formulario4Screen.js
@@ -5,8 +5,9 @@ function Formulario() {
   const [hora, setHora] = useState('');
   const [oxigeno, setOxigeno] = useState('');
   const [combustible, setCombustible] = useState('');
-  const [h2s, setH2s] = useState ('');
-  const [otro, setOtro] = useState ('');
+  const [h2s, setH2s] = useState('');
+  const [otro, setOtro] = useState('');
+  const [formularioVisible, setFormularioVisible] = useState(false);
 
   const handleSubmit = () => {
     console.log('Hora:', hora);
@@ -18,50 +19,230 @@ function Formulario() {
 
   return (
     <View style={styles.contenedor}>
-      <Text style={styles.titulo}> Hora </Text>
-      <TextInput
-        placeholder="Hora"
-        value={hora}
-        onChangeText={(text) => setHora(text)}
-      />
-      <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
-      <TextInput
-        placeholder="Oxígeno"
-        value={oxigeno}
-        onChangeText={(text) => setOxigeno(text)}
-      />
-      <Text style={styles.titulo}> Combustible LEL .0% </Text>
-      <TextInput
-        placeholder="Combustible"
-        value={combustible}
-        onChangeText={(text) => setCombustible(text)}
-      />
-      <Text style={styles.titulo}> H2S </Text>
-      <TextInput
-        placeholder="H2S"
-        value={h2s}
-        onChangeText={(text) => setH2s(text)}
-      />
-      <Text style={styles.titulo}> Otro </Text>
-      <TextInput
-        placeholder="Otro"
-        value={otro}
-        onChangeText={(text) => setOtro(text)}
-      />
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
+      <Button title="Mostrar acuifero guarani" onPress={() => setFormularioVisible(!formularioVisible)} />
+      {formularioVisible && (
+        <View>
+          <Text style={styles.titulo}> Hora </Text>
+          <TextInput
+            placeholder="Hora"
+            value={hora}
+            onChangeText={(text) => setHora(text)}
+          />
+          <Text style={styles.titulo}> Oxígeno 19.5-22.5% </Text>
+          <TextInput
+            placeholder="Oxígeno"
+            value={oxigeno}
+            onChangeText={(text) => setOxigeno(text)}
+          />
+          <Text style={styles.titulo}> Combustible LEL .0% </Text>
+          <TextInput
+            placeholder="Combustible"
+            value={combustible}
+            onChangeText={(text) => setCombustible(text)}
+          />
+          <Text style={styles.titulo}> H2S </Text>
+          <TextInput
+            placeholder="H2S"
+            value={h2s}
+            onChangeText={(text) => setH2s(text)}
+          />
+          <Text style={styles.titulo}> Otro </Text>
+          <TextInput
+            placeholder="Otro"
+            value={otro}
+            onChangeText={(text) => setOtro(text)}
+          />
+        </View>
+      )}
       <Button title="Enviar" onPress={handleSubmit} />
     </View>
+    
   );
 }
 
 export default Formulario;
 
-
 const styles = StyleSheet.create({
-  titulo:{
+  titulo: {
     textAlign: 'center',
     fontSize: 22,
   },
-  contenedor:{
+  contenedor: {
     padding: 10,
   },
-})
+});
